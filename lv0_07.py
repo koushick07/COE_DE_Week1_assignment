17d26d5ce7d51020d02150512e2b4a7e4a5edff1
Koushick <koushickmondal2021@gmail.com>
2024-05-24 12:05:14 +0530

    Add files via upload

---
+++ lv0_07.py
@@ -0,0 +1,11 @@
+from itertools import combinations
+l = int(input())
+s = input().split()
+c = int(input())
+
+leng = len(list(combinations(s,c)))
+data = 0
+for i in combinations(s,c):
+    if "a" in i:
+        data+=1
+print(float("{:.12f}".format(data/leng)))
